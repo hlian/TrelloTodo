c5e1352279cc867a7ab9f40d3aa6bf6060fcaa0d
Hao Lian <hi@haolian.org>
2015-04-07 14:44:12 -0700

    here we go

---
+++ Todo/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Trello {
+    public class TodoItem : INotifyPropertyChanged {
+        public string Title { get; set; }
+       
+        private bool isChecked;
+        public bool IsChecked {
+            get { return isChecked; }
+            set { isChecked = value; OnPropertyChanged("IsChecked"); }
+        }
+
+        public override string ToString() {
+            return string.Format("<TodoItem(Title={0}, IsChecked={1})>", Title, IsChecked);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private void OnPropertyChanged(string propertyName) {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public sealed partial class MainPage : Page {
+        public ObservableCollection<TodoItem> Todos = new ObservableCollection<TodoItem>();
+
+        public MainPage() {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Required;
+
+            Observable
+                // Every time the collection changes...
+                .FromEventPattern<NotifyCollectionChangedEventArgs>(Todos, "CollectionChanged")
+                // ... grab all the TodoItems that were added ...
+                .SelectMany(change => change.EventArgs.NewItems.Cast<TodoItem>())
+                // ... map to +1 if the item IsChecked, otherwise 0; then monitor IsChecked and map to +1/-1 accordingly ...
+                .Select(item => item.GetPropertyValues(x => x.IsChecked).CombinePreviousWithStart(0, (bool previous, bool current) => current.CompareTo(previous)))
+                // ... and finally tally up all +1's and -1's ...
+                .Merge().Scan(0, (sum, delta) => sum + delta)
+                // ... so we can print it out!
+                .Subscribe(delta => Debug.WriteLine("Number of checked items: {0}", delta));
+
+            Todos.Add(new TodoItem { Title = "Eggs" });
+            Todos.Add(new TodoItem { Title = "Airplane tickets" });
+            Todos.Add(new TodoItem { Title = "Feed husky", IsChecked = true });
+
+            ListView.DataContext = this.Todos;
+        }
+    }
+}
---
+++ Todo/NotifyPropertyChangeReactiveExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trello {
+    public static class Extensions {
+        // Returns the values of property (an Expression) as they change,
+        // starting with the current value
+        public static IObservable<TValue> GetPropertyValues<TSource, TValue>(
+            this TSource source, Expression<Func<TSource, TValue>> property)
+            where TSource : INotifyPropertyChanged {
+            MemberExpression memberExpression = property.Body as MemberExpression;
+
+            if (memberExpression == null) {
+                throw new ArgumentException(
+                    "property must directly access a property of the source");
+            }
+
+            string propertyName = memberExpression.Member.Name;
+
+            Func<TSource, TValue> accessor = property.Compile();
+
+            return source.GetPropertyChangedEvents()
+                .Where(x => x.PropertyName == propertyName)
+                .Select(x => accessor(source))
+                .StartWith(accessor(source));
+        }
+
+        // This is a wrapper around FromEvent(PropertyChanged)
+        public static IObservable<PropertyChangedEventArgs>
+            GetPropertyChangedEvents(this INotifyPropertyChanged source) {
+            return Observable.FromEvent<PropertyChangedEventHandler,
+                PropertyChangedEventArgs>(
+                h => (sender, args) => h(args),
+                h => source.PropertyChanged += h,
+                h => source.PropertyChanged -= h);
+        }
+
+        public static IObservable<TAccumulate> CombinePreviousWithStart<T, TAccumulate>(
+            this IObservable<T> observable,
+            TAccumulate start, 
+            Func<T, T, TAccumulate> mappend) {
+            return observable.Scan<T, Tuple<T, TAccumulate>>(Tuple.Create<T, TAccumulate>(default(T), start), (previousTuple, next) => Tuple.Create(next, mappend(previousTuple.Item1, next))).Select(tuple => tuple.Item2);
+        }
+    }
+}
